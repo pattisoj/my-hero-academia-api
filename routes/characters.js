344e2c48096c4b31135f7e8d758a489b0a9990d2
pattisoj <joshpattison23@icloud.com>
2022-09-20 10:52:44 +0100

    Connected character data to route

--- routes/characters.js
+++ routes/characters.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var router = express.Router();
+var data = require("../data/characters.json");
 
 /* GET characters listing. */
 router.get("/", function (req, res, next) {
-  res.send("This route will supply character information");
+  res.json(data.characters);
 });
 
 module.exports = router;
