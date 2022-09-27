2229c9798e9fd09b3aa46de9d554d0453305b9e0
pattisoj <joshpattison23@icloud.com>
2022-09-27 12:20:48 +0100

    Added GET /characters structure tests

--- test/characters.test.js
+++ test/characters.test.js
@@ -1,7 +1,12 @@
 const request = require("supertest");
+const data = require("../data/characters.json");
 
 describe("Characters API Route", () => {
   const url = "http://localhost:3000";
+  const dataLength = data.students.length;
+  // console.log(dataLength);
+  const i = Math.floor(Math.random() * dataLength);
+  // console.log(i);
 
   test("GET /characters", async () => {});
   request(url)
@@ -10,6 +15,14 @@ describe("Characters API Route", () => {
     .set("Content-Type", "application/json")
     .end(function (err, res) {
       expect(res.statusCode).toEqual(200);
+      expect(res.body.length).toBeGreaterThan(0);
+      expect(res.body[i]).toHaveProperty("id");
+      expect(res.body[i]).toHaveProperty("name");
+      expect(res.body[i]).toHaveProperty("name_japanese");
+      expect(res.body[i]).toHaveProperty("other_names");
+      expect(res.body[i]).toHaveProperty("quirk_japanese");
+      expect(res.body[i]).toHaveProperty("quirk_description");
+      expect(res.body[i]).toHaveProperty("class");
       if (err) {
         throw err;
       }
