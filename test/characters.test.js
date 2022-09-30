1a1ce5891a17f3a5830771f928af59c1f1552f17
pattisoj <joshpattison23@icloud.com>
2022-09-30 10:25:16 +0100

    Updated characters tests

--- test/characters.test.js
+++ test/characters.test.js
@@ -8,23 +8,24 @@ describe("Characters API Route", () => {
   const i = Math.floor(Math.random() * dataLength);
   // console.log(i);
 
-  test("GET /characters", async () => {});
-  request(url)
-    .get("/characters")
-    .set("Accept", "application/json")
-    .set("Content-Type", "application/json")
-    .end(function (err, res) {
-      expect(res.statusCode).toEqual(200);
-      expect(res.body.length).toBeGreaterThan(0);
-      expect(res.body[i]).toHaveProperty("id");
-      expect(res.body[i]).toHaveProperty("name");
-      expect(res.body[i]).toHaveProperty("name_japanese");
-      expect(res.body[i]).toHaveProperty("other_names");
-      expect(res.body[i]).toHaveProperty("quirk_japanese");
-      expect(res.body[i]).toHaveProperty("quirk_description");
-      expect(res.body[i]).toHaveProperty("class");
-      if (err) {
-        throw err;
-      }
-    });
+  test("GET /characters", async () => {
+    request(url)
+      .get("/characters")
+      .set("Accept", "application/json")
+      .set("Content-Type", "application/json")
+      .end(function (err, res) {
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.length).toBeGreaterThan(0);
+        expect(res.body[i]).toHaveProperty("id");
+        expect(res.body[i]).toHaveProperty("name");
+        expect(res.body[i]).toHaveProperty("name_japanese");
+        expect(res.body[i]).toHaveProperty("other_names");
+        expect(res.body[i]).toHaveProperty("quirk_japanese");
+        expect(res.body[i]).toHaveProperty("quirk_description");
+        expect(res.body[i]).toHaveProperty("class");
+        if (err) {
+          throw err;
+        }
+      });
+  });
 });
