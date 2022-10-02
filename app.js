67d97a488d762b1896e71421aad94229ddc076de
pattisoj <joshpattison23@icloud.com>
2022-10-02 12:07:33 +0100

    Removed unused imports

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
 var charactersRouter = require("./routes/characters");
 
 const swaggerUi = require("swagger-ui-express");
