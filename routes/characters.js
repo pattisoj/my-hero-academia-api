1748b9faab440c6ca53e5b309e36dead14025b8d
pattisoj <joshpattison23@icloud.com>
2022-10-10 17:38:49 +0100

    Updated characters routes for villains

--- routes/characters.js
+++ routes/characters.js
@@ -2,9 +2,19 @@ var express = require("express");
 var router = express.Router();
 var data = require("../data/characters.json");
 
-/* GET characters listing. */
-router.get("/", function (req, res, next) {
+/* GET all characters. */
+router.get("/students", function (req, res, next) {
   res.json(data.students);
+});
+
+/* GET all characters. */
+router.get("/villains", function (req, res, next) {
+  res.json(data.villains);
+});
+
+/* GET all characters. */
+router.get("/", function (req, res, next) {
+  res.json(data);
 });
 
 module.exports = router;
