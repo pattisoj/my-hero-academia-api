70937afa15594a0c50a977f8794882490e346f48
pattisoj <joshpattison23@icloud.com>
2022-10-13 11:52:09 +0100

    Added error handling

--- routes/characters.js
+++ routes/characters.js
@@ -1,3 +1,4 @@
+var createError = require("http-errors");
 var express = require("express");
 var router = express.Router();
 var data = require("../data/characters.json");
@@ -17,4 +18,17 @@ router.get("/", function (req, res, next) {
   res.json(data);
 });
 
+/* Catch errors */
+router.use(function (req, res, next) {
+  next(createError(404));
+});
+
+router.use(function (err, req, res, next) {
+  // set locals
+  res.locals.message = err.message;
+  res.locals.error = err;
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 module.exports = router;
