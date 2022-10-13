5e8a7554f0dd5af0fcbca325b8b718c7fecd172c
pattisoj <joshpattison23@icloud.com>
2022-10-13 11:51:56 +0100

    Updated error handling

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+  // set locals
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = err;
 
   // render the error page
   res.status(err.status || 500);
