82b268fd8c73bdd4fa221c3f7fa56c284c8701f1
pattisoj <joshpattison23@icloud.com>
2022-10-18 09:42:10 -0400

    Updated documentation CSS

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const swaggerDocument = require("../swagger.json");
 
 /* Documentation styling options */
 var options = {
-  customCss: `.topbar-wrapper img {content:url(https://1000logos.net/wp-content/uploads/2021/11/My-Hero-Academia-Logo.png); width:20vw; height:auto;} .swagger-ui .topbar { background-color: #FFFFFF; margin-bottom: -10vh; }`,
+  customCss: `.topbar-wrapper img {content:url(https://1000logos.net/wp-content/uploads/2021/11/My-Hero-Academia-Logo.png); width:20vw; height:auto;} .swagger-ui .topbar { background-color: #FFFFFF; margin-bottom: -2vh;}`,
   customSiteTitle: "My Hero Academia API",
   customfavIcon: "/favicon.ico",
 };
