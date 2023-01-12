0cf7949152a84f70d319853125fa93cbcd183b96
pattisoj <joshpattison23@icloud.com>
2023-01-12 12:31:45 +0000

    Updated and improved getCharacterByID function

--- models/characters.js
+++ models/characters.js
@@ -5,27 +5,11 @@ function getCharacterByID(id) {
   let searchedCharacter = {};
   let numID = Number(id);
 
-  for (let i = 0; i < data.students.length; i++) {
-    if (numID == data.students[i].id) {
-      searchedCharacter = data.students[i];
-    }
-  }
-
-  for (let i = 0; i < data.villains.length; i++) {
-    if (numID == data.villains[i].id) {
-      searchedCharacter = data.villains[i];
-    }
-  }
-
-  for (let i = 0; i < data.heroes.length; i++) {
-    if (numID == data.heroes[i].id) {
-      searchedCharacter = data.heroes[i];
-    }
-  }
-
-  for (let i = 0; i < data.other.length; i++) {
-    if (numID == data.other[i].id) {
-      searchedCharacter = data.other[i];
+  for (const key in data) {
+    for (let i = 0; i < data[key].length; i++) {
+      if (numID == data[key][i].id) {
+        searchedCharacter = data[key][i];
+      }
     }
   }
 
